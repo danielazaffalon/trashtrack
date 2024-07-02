478008f1b7be92f0c39aead3b8140c12c8daee8e
Daniela Zaffalon <daniela.zaffalon@onemindng.com>
2024-07-02 09:54:09 +0200

    authentication update

--- src/app/pages/private/tab1/tab1.page.ts
+++ src/app/pages/private/tab1/tab1.page.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../../../explore-container/explore-container.component';
+import { Router } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButton, IonButtons } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { logOutOutline } from 'ionicons/icons';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonButtons, IonButton, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router) {
+    addIcons({
+      "log-out-outline": logOutOutline
+    });
+  }
+
+  logOut(){
+    this.authService.logout().then(()=>{
+      this.router.navigateByUrl('/login', { replaceUrl: true });
+    });
+  }
+
 }
--- src/app/pages/public/login/login.page.ts
+++ src/app/pages/public/login/login.page.ts
@@ -1,20 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonButton, IonInput } from '@ionic/angular/standalone';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule, IonInput]
 })
 export class LoginPage implements OnInit {
+	credentials!: FormGroup;
 
-  constructor() { }
+	constructor(
+		private fb: FormBuilder,
+		private authService: AuthService,
+		private router: Router
+	) {}
 
-  ngOnInit() {
-  }
+	// Easy access for form fields
+	get email() {
+		return this.credentials.get('email');
+	}
 
+	get password() {
+		return this.credentials.get('password');
+	}
+
+	ngOnInit() {
+		this.credentials = this.fb.group({
+			email: ['', [Validators.required, Validators.email]],
+			password: ['', [Validators.required]]
+		});
+	}
+
+	async login() {
+
+		const user = await this.authService.login(this.credentials.value);
+
+		if (user) {
+			this.router.navigateByUrl('/tabs', { replaceUrl: true });
+		} else {
+			console.log('Login failed', 'Please try again!');
+		}
+	}
 }
