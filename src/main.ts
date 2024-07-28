792a7a82afd81d30d1f0a14ea4783c3275f09c1c
Nicolás Gutiérrez <nifegupa@gmail.com>
2024-07-28 12:33:19 +0200

    Feat/images in incident form (#8)
    * add dumpster icon in map and tabs icons
    
    * add image attachment to report incident form
    
    * Add delete image from form

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,10 @@ import { environment } from './environments/environment';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+// Call the element loader before the bootstrapModule/bootstrapApplication call
+defineCustomElements(window);
 
 if (environment.production) {
   enableProdMode();
